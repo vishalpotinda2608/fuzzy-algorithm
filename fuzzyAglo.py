023448ecfa4df7564a22ba17096f35c46dc60fcb
vishalpotinda2608 <vishalpotinda2608@gmail.com>
2023-04-06 09:41:09 +0530

    integrate cosine logic

--- fuzzyAglo.py
+++ fuzzyAglo.py
@@ -1,42 +1,65 @@
+
 from fuzzywuzzy import fuzz,process
-def get_ratio(term, list_names, min_score=0):
-    max_score = 1
-    max_name = ""
-    for term2 in list_names:
-        score = fuzz.ratio(term, term2)
-        if (score > min_score) & (score > max_score):
-            max_name = term2
-            max_score = score
-    return (max_name, max_score)
 
-def get_partial_ratio(term, list_names, min_score=0):
-    max_score = 1
-    max_name = ""
+from nltk.tokenize import word_tokenize
+import nltk
+nltk.download('stopwords')
+nltk.download('punkt')
+from numpy.linalg import norm
+import numpy as np
+
+def get_score(term, list_names):
     for term2 in list_names:
-        score = fuzz.partial_ratio(term, term2)
-        if (score > min_score) & (score > max_score):
-            max_name = term2
-            max_score = score
-    return (max_name, max_score)
+            X = term
+            Y=["Rodrigu"]
+            X_list = word_tokenize(X[0])
+            Y_list = word_tokenize(Y[0])
+            sw = nltk.corpus.stopwords.words('english')
+            X_Set = {w for w in X_list if not w in sw}
+            y_Set = {w for w in Y_list if not w in sw}
+
+            l1 = []
+            l2 = []
+            rvector = X_Set.union(y_Set)
+
+            for w in rvector:
+                if w in rvector:
+                    if w in X_Set:
+                        l1.append(1)
+                    else:
+                        l1.append(0)
+                    if w in y_Set:
+                        l2.append(1)
+                    else:
+                        l2.append(0)
+            c =0
+
+            for i in range(len(rvector)):
+                c+= l1[i] * l2[i]
+
+            max_score =np.dot(l1,l2)/((norm(l1)*norm(l2))**0.5) # 2.3 sec
+
+            # max_score = np.dot(l1,l2)/(norm(l1)*norm(l2)) # 20 sec
+            # max_score=c/(sum(l1)*sum(l2)**0.5) #13.30 s
+            # max_score=cosine_similarity(l1,l2)
+            return (Y[0],max_score)
 
 
-def get_partial_sort_ratio(term, list_names, min_score=0):
-    max_score = 1
-    max_name = ""
-    for term2 in list_names:
-        score = fuzz.partial_token_sort_ratio(term, term2)
-        if (score > min_score) & (score > max_score):
-            max_name = term2
-            max_score = score
-    return (max_name, max_score)
+def cosine_similarity(x, y):
+    # Ensure length of x and y are the same
+    if len(x) != len(y):
+        return None
 
-def get_partial_set_ratio(term, list_names, min_score=0):
-    max_score = 1
-    max_name = ""
-    for term2 in list_names:
-        score = fuzz.partial_token_set_ratio(term, term2)
-        if (score > min_score) & (score > max_score):
-            max_name = term2
-            max_score = score
-    return (max_name, max_score)
+    # Compute the dot product between x and y
+    dot_product = np.dot(x, y)
+
+    # Compute the L2 norms (magnitudes) of x and y
+    magnitude_x = np.sqrt(np.sum(x.pow(2)))
+    magnitude_y = np.sqrt(np.sum(y.pow(2)))
+
+    # Compute the cosine similarity
+    cosine_similarity = dot_product / (magnitude_x * magnitude_y)
+
+    return cosine_similarity
+
 
--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+
 import cx_Oracle
 import pandas as pd
 import fuzzyAglo as fuzz
 from flask import Flask,jsonify,request
+import time
+from sklearn.feature_extraction.text import CountVectorizer
 
 app=Flask(__name__)
 
@@ -15,34 +18,46 @@ def fetchData():
     cur = con.cursor()
     cur.execute("SELECT FIRSTNAME FROM WATCHLISTEXTERNALDATA")
     watch_list = cur.fetchall()
+    # watch_df = pd.DataFrame(watch, columns=["FIRSTNAME"]).dropna()
+    # watch_list =watch_df.values.tolist()
+
     cur.execute("SELECT ACC_NAME FROM CUSTOMERSHISTORY")
     customer_history = cur.fetchall()
+    # watch_df = pd.DataFrame(customer, columns=["ACC_NAME"]).dropna()
+    # customer_history = watch_df.values.tolist()
+
+    # X = CountVectorizer().fit_transform(watch).toarray()
+    # Y=CountVectorizer().fit_transform(customer).toarray()
+    #
 
     dict_list = []
-    for name in watch_list:
-        match = fuzz.get_ratio(name, customer_history, 30)
+
+    start=time.time()
+    for name in  customer_history:
+        match = fuzz.get_score(name, watch_list)
         dict_ = {}
-        dict_.update({"First_Name": name})
+        dict_.update({"First_Name": name[0]})
         dict_.update({"ACC_Name": match[0]})
         dict_.update({"Score": match[1]})
         dict_list.append(dict_)
+    end=time.time()
+    print(end-start)
 
     df = pd.DataFrame(dict_list)
     matched_data = df.nlargest(n=5, columns=['Score'])
     con.commit()
     cur.close()
     con.close()
-    print(matched_data)
     return matched_data
 
 @app.route('/fuzzy',methods=['GET'])
 def getData():
      db_res=fetchData()
-     items=db_res.values.tolist()
+     print(db_res)
+
      data = {
          "statusCode":200,
-         "msg": "SUCCESS",
-         "data":items
+         "msg": "SUCCESS"
      }
      return jsonify(data)
 
@@ -52,54 +67,4 @@ def hello_world():
 
 
 if __name__=="__main__":
-    app.run(debug=True,port=8000)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#
-# try:
-#     con = cx_Oracle.connect('moz_aml/moz_aml@3.111.8.162:1521/xe')
-# except cx_Oracle.DatabaseError as er:
-#     print('There is an error in the Oracle database:', er)
-# else:
-#     try:
-#         cur = con.cursor()
-#         cur.execute("SELECT FIRSTNAME FROM WATCHLISTEXTERNALDATA")
-#         watch_list=cur.fetchall()
-#         cur.execute("SELECT ACC_NAME FROM CUSTOMERSHISTORY")
-#         customer_history=cur.fetchall()
-#
-#         dict_list = []
-#         for name in watch_list:
-#             match = fuzz.get_ratio(name, customer_history, 30)
-#             dict_ = {}
-#             dict_.update({"First_Name": name})
-#             dict_.update({"ACC_Name": match[0]})
-#             dict_.update({"Score": match[1]})
-#             dict_list.append(dict_)
-#
-#         df = pd.DataFrame(dict_list)
-#         matched_data = df.nlargest(n=5, columns=['Score'])
-#         print(matched_data)
-#
-#     except cx_Oracle.DatabaseError as er:
-#         print('There is an error in the Oracle database:', er)
-#     except Exception as er:
-#         print('Error:' + str(er))
-#     finally:
-#         if cur:
-#             cur.close()
-# finally:
-#     if con:
-#         con.close()
+    app.run(debug=True,port=5000)
