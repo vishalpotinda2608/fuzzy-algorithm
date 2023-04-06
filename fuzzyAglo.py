510f57c4028ae97a0a8ee58e298738c6603ae6ed
vishalpotinda2608 <vishalpotinda2608@gmail.com>
2023-04-06 21:33:58 +0530

    jaro_winkler_distance

--- fuzzyAglo.py
+++ fuzzyAglo.py
@@ -9,57 +9,84 @@ from numpy.linalg import norm
 import numpy as np
 
 def get_score(term, list_names):
+    print(len(list_names))
     for term2 in list_names:
             X = term
-            Y=["Rodrigu"]
-            X_list = word_tokenize(X[0])
-            Y_list = word_tokenize(Y[0])
-            sw = nltk.corpus.stopwords.words('english')
-            X_Set = {w for w in X_list if not w in sw}
-            y_Set = {w for w in Y_list if not w in sw}
+            Y=term2
+            max_score=jaro_winkler_distance(X[0],Y[0])
+            # X_list = word_tokenize(X[0])
+            # Y_list = word_tokenize(Y[0])
+            # sw = nltk.corpus.stopwords.words('english')
+            # X_Set = {w for w in X_list if not w in sw}
+            # y_Set = {w for w in Y_list if not w in sw}
+            #
+            # l1 = []
+            # l2 = []
+            # rvector = X_Set.union(y_Set)
+            #
+            # for w in rvector:
+            #     if w in rvector:
+            #         if w in X_Set:
+            #             l1.append(1)
+            #         else:
+            #             l1.append(0)
+            #         if w in y_Set:
+            #             l2.append(1)
+            #         else:
+            #             l2.append(0)
+            # c =0
+            #
+            # for i in range(len(rvector)):
+            #     c+= l1[i] * l2[i]
+            #
+            # max_score =np.dot(l1,l2)/((norm(l1)*norm(l2))**0.5) # 2.3 sec
 
-            l1 = []
-            l2 = []
-            rvector = X_Set.union(y_Set)
-
-            for w in rvector:
-                if w in rvector:
-                    if w in X_Set:
-                        l1.append(1)
-                    else:
-                        l1.append(0)
-                    if w in y_Set:
-                        l2.append(1)
-                    else:
-                        l2.append(0)
-            c =0
-
-            for i in range(len(rvector)):
-                c+= l1[i] * l2[i]
-
-            max_score =np.dot(l1,l2)/((norm(l1)*norm(l2))**0.5) # 2.3 sec
-
-            # max_score = np.dot(l1,l2)/(norm(l1)*norm(l2)) # 20 sec
-            # max_score=c/(sum(l1)*sum(l2)**0.5) #13.30 s
-            # max_score=cosine_similarity(l1,l2)
             return (Y[0],max_score)
 
 
-def cosine_similarity(x, y):
-    # Ensure length of x and y are the same
-    if len(x) != len(y):
-        return None
+def jaro_winkler_distance(s1, s2, prefix_weight=0.1):
+    # Compute Jaro distance
+    jaro_sim = 0.0
+    if s1 == s2:
+        jaro_sim = 1.0
+    else:
+        # Find matching characters
+        len_s1 = len(s1)
+        len_s2 = len(s2)
+        match_distance = max(len_s1, len_s2) // 2 - 1
+        s1_matches = [False] * len_s1
+        s2_matches = [False] * len_s2
+        matches = 0
+        for i in range(len_s1):
+            start = max(0, i - match_distance)
+            end = min(i + match_distance + 1, len_s2)
+            for j in range(start, end):
+                if not s2_matches[j] and s1[i] == s2[j]:
+                    s1_matches[i] = True
+                    s2_matches[j] = True
+                    matches += 1
+                    break
+        if matches > 0:
+            # Compute transpositions
+            transpositions = 0
+            j = 0
+            for i in range(len_s1):
+                if s1_matches[i]:
+                    while not s2_matches[j]:
+                        j += 1
+                    if s1[i] != s2[j]:
+                        transpositions += 1
+                    j += 1
+            transpositions //= 2
+            # Compute Jaro distance
+            jaro_sim = (matches / len_s1 + matches / len_s2 + (matches - transpositions) / matches) / 3
 
-    # Compute the dot product between x and y
-    dot_product = np.dot(x, y)
-
-    # Compute the L2 norms (magnitudes) of x and y
-    magnitude_x = np.sqrt(np.sum(x.pow(2)))
-    magnitude_y = np.sqrt(np.sum(y.pow(2)))
-
-    # Compute the cosine similarity
-    cosine_similarity = dot_product / (magnitude_x * magnitude_y)
-
-    return cosine_similarity
-
-
+    # Compute Jaro-Winkler distance
+    prefix_len = 0
+    for i in range(min(len(s1), len(s2))):
+        if s1[i] == s2[i]:
+            prefix_len += 1
+        else:
+            break
+    jaro_winkler_sim = jaro_sim + prefix_weight * prefix_len * (1 - jaro_sim)
+    return jaro_winkler_sim
--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import pandas as pd
 import fuzzyAglo as fuzz
 from flask import Flask,jsonify,request
 import time
-from sklearn.feature_extraction.text import CountVectorizer
 
 app=Flask(__name__)
 
@@ -14,56 +13,80 @@ def getConnection():
     return connection
 
 def fetchData():
+    start=time.time()
     con=getConnection()
     cur = con.cursor()
-    cur.execute("SELECT FIRSTNAME FROM WATCHLISTEXTERNALDATA")
-    watch_list = cur.fetchall()
-    # watch_df = pd.DataFrame(watch, columns=["FIRSTNAME"]).dropna()
-    # watch_list =watch_df.values.tolist()
 
     cur.execute("SELECT ACC_NAME FROM CUSTOMERSHISTORY")
-    customer_history = cur.fetchall()
-    # watch_df = pd.DataFrame(customer, columns=["ACC_NAME"]).dropna()
-    # customer_history = watch_df.values.tolist()
+    customer = cur.fetchall()
+    watch_df = pd.DataFrame(customer, columns=["ACC_NAME"]).dropna()
+    customer_history = watch_df.values.tolist()
 
-    # X = CountVectorizer().fit_transform(watch).toarray()
-    # Y=CountVectorizer().fit_transform(customer).toarray()
-    #
+    cur.execute("SELECT FIRSTNAME FROM WATCHLISTEXTERNALDATA")
+    watch_list = cur.fetchall()
+
+    end = time.time()
+    print("db time:", end - start)
+    print(len(customer_history))
+    print(len(watch_list))
 
     dict_list = []
 
     start=time.time()
     for name in  customer_history:
         match = fuzz.get_score(name, watch_list)
+        # if match[1] > 0:
+        #     save_watcheslist_data(match[0],match[1])
+        # else:
+        #     pass
+
         dict_ = {}
-        dict_.update({"First_Name": name[0]})
-        dict_.update({"ACC_Name": match[0]})
-        dict_.update({"Score": match[1]})
+        dict_.update({"ACC_Name": name[0]})
+        dict_.update({"First_Name": match[0]})
+        dict_.update({"Similarity": match[1]})
         dict_list.append(dict_)
     end=time.time()
-    print(end-start)
+    print("cosine time:",end-start)
 
     df = pd.DataFrame(dict_list)
-    matched_data = df.nlargest(n=5, columns=['Score'])
+    matched_data = df.nlargest(n=5, columns=['Similarity'])
     con.commit()
     cur.close()
     con.close()
     return matched_data
 
+
+def save_watcheslist_data():
+    con=getConnection()
+    cur=con.cursor()
+    cur.execute("INSERT INTO WATCHLISTMATCHDETAILS(ID,CUSTTYPE,TYPE,CUSTID,CUSTHISTORYID,MATCHKEYS,WATCHLISTMATCHKEYS,SCORE,TOTALSCORE,PERCENTAGESCORE,MATCHVALUES,MATCHWATCHLISTVALUES,WATCHLISTID,ACCID) VALUES ('8329241021','11','EXTERNAL','15760','NA','ACC_NAME','TITLE,FIRSTNAME,MIDDLENAME,LASTNAME,DATATYPE','99.99','80','80','Vishal','vishal the fraduer','26088','PT877393907')")
+    watch_details = cur.fetchall()
+    print(watch_details)
+
 @app.route('/fuzzy',methods=['GET'])
 def getData():
-     db_res=fetchData()
-     print(db_res)
+     matched_data_df=fetchData()
+     print(matched_data_df)
+     matched_list=matched_data_df.values.tolist()
+     result=[]
+     for i in range(len(matched_list)):
+         match_dic={}
+         match_dic.update({"ACC_Name":matched_list[i][0]})
+         match_dic.update({"FIRSTNAME":matched_list[i][1]})
+         match_dic.update({"Similarity":matched_list[i][2]})
+         result.append(match_dic)
 
      data = {
          "statusCode":200,
-         "msg": "SUCCESS"
+         "msg": "SUCCESS",
+         "data":result
      }
      return jsonify(data)
 
 @app.route('/',methods=['GET'])
 def hello_world():
     return "Hello World"
+
 
 
 if __name__=="__main__":
